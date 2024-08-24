27e2ed2bc15761aa7a79a0b0cb1940aaf5948870
April Twenty <atcahriya.nna@gmail.com>
2024-08-24 16:45:41 +0700

    Answer Ex.1-Ex.6

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,17 @@
 // Start coding here
+let add = (a,b) => {
+    return a + b;
+}
+//console.log(add(10,5));
+let substract = (a,b) => {
+    return a - b;
+}
+//console.log(substract(10,5));
+let multiply = (a,b) => {
+    return a * b;
+}
+//console.log(multiply(10,5));
+let divide = (a,b) => {
+    return a / b;
+}
+//console.log(divide(10,5));
